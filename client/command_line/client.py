7853d2e575ae85dc211a68c65f3d3f780ea8cef9
Bohdan <zaporozhec.bogdan@gmail.com>
2022-06-02 13:04:00 +0300

    test2

--- client/command_line/client.py
+++ client/command_line/client.py
@@ -1,10 +1,18 @@
 import socket
+from pynput.keyboard import Key, Controller
+
 
 sock = socket.socket()
 
-ip = input('Enter ip ')
-port = input('Enter port ')
-if port.isdigit():
-    port = int(port)
+host = input('enter host: ')
 
-sock.connect((ip, port))
+while True:
+    try:
+        port = input('enter port: ')
+        port = int(port)
+        break
+    except:
+        print('wrong port try again')
+
+
+conn = sock.connect((host, port))
--- server/server.py
+++ server/server.py
@@ -1,14 +1,43 @@
 import socket
+import threading
 
 
-sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-IP = socket.gethostbyname(socket.gethostname())
-sock.bind((IP, 9090))
-hostname = socket.gethostname()
-print(IP)
+class Client(object):
+    def __init__(self, _socket, address):
+        self.socket = _socket
+        self.address = address
 
 
-sock.listen(1)
-while True:
-    conn, addr = sock.accept()
-    print(conn, addr)
+class Server:
+    def __init__(self, host, port):
+        self.clients = []
+        self.host = host
+        self.port = port
+        self.sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        self.sock.bind((self.host, self.port))
+        print(f'Started server at {self.host}:{self.port}')
+        self.server_listener = threading.Thread(target=self.listen)
+
+    def start(self):
+        self.server_listener.start()
+        print('started')
+
+    def listen(self):
+        self.sock.listen()
+        print(self.sock)
+        while True:
+            client_socket, address = self.sock.accept()
+            self.clients.append(Client(_socket=client_socket, address=address))
+            print(address, 'connected')
+
+
+if __name__ == '__main__':
+
+    IP = socket.gethostbyname(socket.gethostname())
+    PORT = 9090
+    server = Server(IP, PORT)
+
+    try:
+        server.start()
+    except Exception as error:
+        print(error)
