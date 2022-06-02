7b9531b7ea8cedafa302d7f275f3b646a8078321
Bohdan <zaporozhec.bogdan@gmail.com>
2022-06-02 12:08:49 +0300

    start

---
+++ client/command_line/client.py
@@ -0,0 +1,5 @@
+import socket
+
+sock = socket.socket()
+
+sock.connect(('172.24.224.1', 9090))
--- server/server.py
+++ server/server.py
@@ -1,7 +1,14 @@
 import socket
 
+def getMyIp():
+    s = socket.socket(socket.AF_INET, socket.SOCK_DGRAM) #Создаем сокет (UDP)
+    s.setsockopt(socket.SOL_SOCKET, socket.SO_BROADCAST, 1) # Настраиваем сокет на BROADCAST вещание.
+    s.connect(('<broadcast>', 0))
+    return s.getsockname()[0]
 
-sock = socket.socket()
+print(getMyIp())
+
+sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 sock.bind(('', 9090))
 hostname = socket.gethostname()
 ip = socket.gethostbyname_ex(hostname)
